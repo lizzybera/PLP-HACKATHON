ca478335a66eb7f3b9df328e72e26a59db9ef51a
Elizabeth Okorie <elizabethokorie407@gmail.com>
2023-10-19 15:23:06 +0100

    jb

--- src/pages/static/Header.tsx
+++ src/pages/static/Header.tsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
+import {AiOutlineMenu} from "react-icons/ai"
+import { useState } from "react";
 
 const Header = () => {
+  const [show,setShow] = useState<boolean>(false)
+
+  const Change = () => {
+    setShow(!show)
+  }
   return (
     <>
       <div className="w-[100%] h-[70px] flex items-center justify-center ">
@@ -10,13 +17,14 @@ const Header = () => {
             <p>WeatherData</p>
           </div>
           <div>
+            <AiOutlineMenu size={23} className="text-white hidden screen425:flex" />
             <Link to={"/sign-in"}>
-              <button className="px-4 py-2 hover:bg-[darkorange] rounded-md bg-slate-400 text-white text-[18px] font-bold duration-[350ms] mr-5">
+              <button className="px-4 py-2 hover:bg-[darkorange] rounded-md bg-slate-400 text-white text-[18px] font-bold duration-[350ms] mr-5 screen425:hidden">
                 login
               </button>
             </Link>
             <Link to={"/register"}>
-              <button className="px-4 py-2 hover:bg-[darkorange] rounded-md bg-slate-400 text-white text-[18px] font-bold duration-[350ms]">
+              <button className="px-4 py-2 hover:bg-[darkorange] rounded-md bg-slate-400 text-white text-[18px] font-bold duration-[350ms]   screen425:hidden">
                 sign up
               </button>
             </Link>
